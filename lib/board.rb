5b5d82799385f326180320146aa9aefa6d5bdb8b
Robin Unger <robin.e.unger@gmail.com>
2019-04-29 16:14:59 -0500

    Add Board#to_s

--- lib/board.rb
+++ lib/board.rb
@@ -4,4 +4,13 @@ class Board
     @grid = Array.new(col) {Array.new(row, " ")}
   end
 
+  def to_s
+    output = ""
+    output << "\n"
+    grid.each do |row|
+      output << row.to_s + "\n"
+    end
+    output << "\n"
+  end
+
 end

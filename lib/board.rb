fdeaed234c92e7a07b333a02edc9bde435e551d8
Robin Unger <robin.e.unger@gmail.com>
2019-04-30 16:37:19 -0500

    add function to print board to console

--- lib/board.rb
+++ lib/board.rb
@@ -1,14 +1,25 @@
 class Board
   attr_reader :grid
   def initialize(col, row)
-    @grid = Array.new(col) {Array.new(row, " ")}
+    @grid = Array.new(col) {Array.new(row, "")}
   end
 
   def to_s
-    output = ""
-    output << "\n"
-    grid.each do |row|
-      output << row.to_s + "\n"
+    output = "\n   "
+    output << "   a     b     c     d     e     f     g     h\n   "
+    grid.size.times { output << "######" }
+    output << "#\n"
+
+    grid.each_with_index do |row, i|
+      output << " #{i+1} "
+      row.each do |cell|
+        cell.empty? ? output << "#     " : output << "#  #{cell.name}  "
+      end
+
+      output << "#\n   "
+      grid.size.times { output << "######" }
+      output << "#\n"
+
     end
     output << "\n"
   end

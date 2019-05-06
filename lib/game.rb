477f1c8353af023d635861d4f0546c0c7c68ee96
Robin Unger <robin.e.unger@gmail.com>
2019-05-06 16:41:19 -0500

    Add function to move pieces

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,23 @@
 class Game
+  attr_reader :board
   def initialize(board, player1, player2)
     @board = board
     @player1, @player2 = player1, player2
   end
-  
+
+  def move_piece(starting, ending)
+    board[starting], board[ending] = board[ending], board[starting]
+  end
+
+  def valid_move?(coordinates)
+    coordinates.all? {|value| value.between?(0,7)}
+  end
+
+  def get_possible_moves(piece, position)
+    position = board.chess_notation_to_coordinates(position)
+    possible_moves = piece.moves[0..-1] #duplicate piece move list
+    possible_moves.map! { |y,x| [y+position[0], x+position[1]] }
+    possible_moves.select! { |coordinates| valid_move?(coordinates) }
+    possible_moves.map { |coordinates| board.coordinates_to_chess_notation(coordinates) }
+  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,4 +1,50 @@
 require "game.rb"
 
 describe Game do
+  describe "#get_possible_moves" do 
+    it "returns a list of valid moves for a knight" do
+      p1, p2 = Player.new("p1", :white), Player.new("p2", :black)
+      board = Board.new
+      board.clear
+      game = Game.new(board, p1, p2)
+      knight = Piece.new(:knight, :white)
+
+      board[:b2] = knight
+      moves = game.get_possible_moves(knight, :b2)
+      expect(moves.sort).to eql([:a4, :c4, :d1, :d3])
+    end
+
+    it "returns a list of valid moves for a rook" do
+      p1, p2 = Player.new("p1", :white), Player.new("p2", :black)
+      board = Board.new
+      board.clear
+      game = Game.new(board, p1, p2)
+      rook = Piece.new(:rook, :white)
+
+      board[:b2] = rook 
+      moves = game.get_possible_moves(rook, :b2)
+      expect(moves.sort).to eql([:a2, :b1, :b3, :b4, :b5, :b6, :b7, :b8, :c2, :d2, :e2, :f2, :g2, :h2])
+    end
+  end
+
+  describe "#move_piece" do
+    it "moves knight from b1 to c3" do
+      board = Board.new
+      board.clear
+      p1, p2 = Player.new("p1", :white), Player.new("p2", :black)
+      game = Game.new(board, p1, p2)
+      knight = Piece.new(:knight, :white)
+
+      board[:b1] = knight
+      expect(board[:b1]).to eql(knight)
+      expect(board[:c3]).to eql("")
+
+      game.move_piece(:b1, :c3)
+
+      expect(board[:b1]).to eql("")
+      expect(board[:c3]).to eql(knight)
+    end
+
+  end
+
 end

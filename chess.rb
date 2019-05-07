3325d00bae428f3ee814f8971da1e6742cacca23
Robin Unger <robin.e.unger@gmail.com>
2019-05-06 20:45:04 -0500

    add moves for pawns

--- chess.rb
+++ chess.rb
@@ -1,6 +1,9 @@
 #!/usr/bin/env ruby
 require "./lib/board.rb"
 require "./lib/piece.rb"
+require "./lib/player.rb"
 
+player1, player2 = Player.new("Player 1", :white), Player.new("Player 2", :black)
 board = Board.new
+
 puts board
--- lib/piece.rb
+++ lib/piece.rb
@@ -18,3 +18,21 @@ class Piece
     SYMBOLS[(type.to_s + color.to_s).to_sym]
   end
 end
+
+# create a seperate pawn class to keep track of the pawn's state (if it has moved or not) and direction of it's moves
+class Pawn < Piece
+  def initialize(color)
+    @type = :pawn
+    @color = color
+  end
+
+  def moves
+    case color
+    when :white
+      [1,0]
+    when :black
+      [-1,0]
+    end
+  end
+
+end
